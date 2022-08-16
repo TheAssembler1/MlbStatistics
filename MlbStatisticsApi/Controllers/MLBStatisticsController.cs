1e7de50191b98363826b6c1e094b118da47a04ab
TheAssembler1 <lewisnoah592@gmail.com>
2022-08-16 14:39:37 -0500

    added get endpoint for all current players

--- MlbStatisticsApi/Controllers/MLBStatisticsController.cs
+++ MlbStatisticsApi/Controllers/MLBStatisticsController.cs
@@ -1,9 +1,25 @@
-﻿using System.Web.Http;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MLBWebScraper;
 
 namespace MlbStatisticsApi.Controllers
 {
-    public class MLBStatisticsController : ApiController
+    [ApiController]
+    [Route("[controller]/GetAllPlayers")]
+    public class MlbStatisticsController : ControllerBase
     {
-       
+        [HttpGet(Name = "GetAllPlayers")]
+        public async Task<List<List<string>>> GetAllPlayers()
+        {
+            List<List<string>> list = await WebScraper.GetAllPlayers();
+
+            foreach (var item in list)
+            {
+                foreach (var player in item)
+                    Console.WriteLine(player);
+            }
+
+            return list;
+        }
     }
 }
